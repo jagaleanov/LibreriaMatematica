1e1da1ae9ab8699de0d7b141aa2b2cc73913f596
jagaleanov <54491111+jagaleanov@users.noreply.github.com>
2020-07-20 01:16:56 -0500

    Add files via upload

---
+++ modulo_matematicas.py
@@ -0,0 +1,13 @@
+# Crear un módulo de funciones aritméticas que realicen las operaciones de 
+# suma, resta, multiplicación, división y potencia de enteros, 
+# escribir un programa que importe este módulo y use estas funciones para 
+# operar con números capturados desde el telado.
+
+
+import mathmodule
+a = int(input("ingrese un numero: "))
+b = int(input("ingrese un segundo numero: "))
+print(a +"+"+b+"="+mymodule.suma(a,b))
+print(a +"-"+b+"="+mymodule.resta(a,b))
+print(a +"*"+b+"="+mymodule.multiplica(a,b))
+print(a +"/"+b+"="+mymodule.divide(a,b))
